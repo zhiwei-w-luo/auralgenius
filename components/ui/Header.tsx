02605edbfce2e395f185d06380d4a582b371e7cb
zhiwei-w-luo <zhiwei.w.luo@gmail.com>
2024-02-13 21:44:32 +0800

    rename Nodes To AuralGenius

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
             className="h-5 w-5 md:h-8 md:w-8"
           />
           <h1 className="text-xl font-medium text-[#25292F] md:text-3xl">
-            NotesGPT
+          AuralGenius
           </h1>
         </Link>
         {/* buttons */}
