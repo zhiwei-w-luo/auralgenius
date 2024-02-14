d72cf65cfd725c2a94d6629ee89c0f5195a396e4
zhiwei-w-luo <zhiwei.w.luo@gmail.com>
2024-02-14 10:51:54 +0800

    fix readme Text Error

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-<a href="https://usenotesgpt.com/">
-  <img alt="NotesGPT – AI-powered voice note taking in seconds." src="/public/images/og-image.png">
-  <h1 align="center">notesGPT</h1>
+<a href="https://AuralGenius.com/">
+  <img alt="AuralGenius – AI-powered voice note taking in seconds." src="/public/images/og-image.png">
+  <h1 align="center">AuralGenius</h1>
 </a>
 
 <p align="center">
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,11 +5,11 @@ import Footer from '@/components/ui/Footer';
 import { Toaster } from 'react-hot-toast';
 import { Analytics } from '@vercel/analytics/react';
 
-let title = 'notesGPT - Take notes with your voice';
+let title = 'AuralGenius - Take notes with your voice';
 let description = 'Generate action items from your notes in seconds';
-let url = 'https://usenotesgpt.com';
-let ogimage = 'https://usenotesgpt.com/images/og-image.png';
-let sitename = 'usenotesgpt.com';
+let url = 'https://auralgenius.com';
+let ogimage = 'https://auralgenius.com/images/og-image.png';
+let sitename = 'AuralGenius.com';
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
